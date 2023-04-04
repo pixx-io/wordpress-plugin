65c7d3c4a9275226365cb0680b6c2328aeeb7282
48DESIGN - Constantin Groß <cg@48design.com>
2023-04-05 00:00:01 +0200

    update plugin header

--- pixxio.php
+++ pixxio.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: pixx.io
- * Version: 0.1.3
+ * Version: 0.1.4
  * Description: The official WordPress plugin for pixx.io. Bring Digital Asset Management to your WordPress sites by importing assets into your media library.
  * Plugin URI: https://www.pixx.io/
  * Author: 48DESIGN GmbH
