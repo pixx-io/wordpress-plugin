af849f20e63680c1e0ef6d713918b0d688a72a2b
48DESIGN - Constantin Groß <cg@48design.com>
2023-04-04 10:10:04 +0200

    added dist scripts

---
+++ src/dist-post.js
@@ -0,0 +1,31 @@
+const AdmZip = require("adm-zip");
+const fs = require('fs');
+const path = require('path');
+const packageJson = JSON.parse(fs.readFileSync('package.json'));
+const pluginSlug = path.basename(process.cwd());
+
+const inputFile = `./${pluginSlug}.zip`;
+const zip = new AdmZip(inputFile);
+
+// Create a new folder in the zip file with the name of the plugin slug
+const baseFolder = `${pluginSlug}/`;
+zip.addFile(baseFolder, Buffer.alloc(0));
+
+// Move all the files in the root of the zip file into the new folder
+const zipEntries = zip.getEntries();
+zipEntries.slice().forEach((entry) => {
+    // skip newly added folder and files
+    if( entry.entryName.startsWith(baseFolder) ) return;
+
+    zip.addFile(`${baseFolder}${entry.entryName}`, entry.getData(), '', entry.comment);
+    zip.deleteFile(entry.entryName);
+});
+
+const outputFileName = `${pluginSlug}-v${packageJson.version}.zip`;
+const distPath = path.join(process.cwd(), 'dist');
+if (!fs.existsSync(distPath)) {
+  fs.mkdirSync(distPath);
+}
+
+zip.writeZip(path.join(distPath, outputFileName));
+fs.unlinkSync(inputFile);
---
+++ src/dist-pre.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const packageJson = JSON.parse(fs.readFileSync('package.json')); // replace with your actual package.json file path
+
+const pluginSlug = path.basename(process.cwd());
+const mainFile = `${pluginSlug}.php`;
+
+if (!fs.existsSync(mainFile)) {
+  throw new Error(`Main PHP file not found in ${process.cwd()}`);
+}
+
+let contents = fs.readFileSync(mainFile, 'utf8');
+contents = contents.replace(/(Version:\s*)([\d.]+)/i, `$1${packageJson.version}`);
+fs.writeFileSync(mainFile, contents);
